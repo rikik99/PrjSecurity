bbcdd3d8097aef69b24e881d3a6b95bc98941a3e
riki <youki08@naver.com>
2024-05-09 18:08:53 +0900

    security

---
+++ src/main/java/com/green/service/UserService.java
@@ -0,0 +1,26 @@
+package com.green.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.green.dto.UserDto;
+import com.green.entity.User;
+import com.green.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(UserDto dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }// dto 안에 있는 data 를 저장하고	
+     //저장된 id return
+}
